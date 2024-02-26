2a55922e13b134a322acfbec4f2dc7e0a5e1ba4f
Princewill Elebhose <princewillfidelis1@gmail.com>
2024-02-26 23:47:44 +0100

    Add a bubble sort algrithm to sort and array n ascending order

---
+++ 0-bubble_sort.c
@@ -0,0 +1,39 @@
+#include "sort.h"
+
+/**
+ * bubble_sort - This function uses bubble sort algorithm to sort an array
+ * @array: This is the array to be sorted
+ * @size: the size of the array
+ * Return: nothing
+ */
+
+void bubble_sort(int *array, size_t size)
+{
+	size_t i, k, temp, swapped;
+
+	if (size < 2 || array == NULL)
+	{
+		return;
+	}
+
+	for (k = 0; k < size - 1; k++)
+	{
+		swapped = 0;
+		for (i = 0; i < size - 1 - k; i++)
+		{
+			if (array[i] > array[i + 1])
+			{
+				temp = array[i];
+				array[i] = array[i + 1];
+				array[i + 1] = temp;
+				swapped++;
+				print_array(array, size);
+			}
+		}
+		if (swapped == 0)
+		{
+			return;
+		}
+	}
+
+}
